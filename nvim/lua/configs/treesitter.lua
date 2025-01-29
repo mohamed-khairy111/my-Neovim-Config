faffde5ece97155d8df3702fd3d90753505f708e
mohamed <mohamedkhairy0101@gmail.com>
2025-01-29 18:48:55 +0200

    configs

---
+++ nvim/lua/configs/treesitter.lua
@@ -0,0 +1,38 @@
+local options = {
+    ensure_installed = {
+        "bash",
+        "c",
+        "cmake",
+        "cpp",
+        "fish",
+        "go",
+        "gomod",
+        "gosum",
+        "gotmpl",
+        "gowork",
+        "haskell",
+        "lua",
+        "luadoc",
+        "make",
+        "markdown",
+        "odin",
+        "printf",
+        "python",
+        "toml",
+        "vim",
+        "vimdoc",
+        "yaml",
+        "javascript",
+        "typescript",
+        "ninja",
+    },
+
+    highlight = {
+        enable = true,
+        use_languagetree = true,
+    },
+
+    indent = { enable = true },
+}
+
+require("nvim-treesitter.configs").setup(options)
